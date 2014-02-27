59ed27eb630e8f528f1c979ea335fe1fad869c96
jpmonettas <jpmonettas@gmail.com>
2014-02-27 11:07:13 -0200

    Cleanning examples Adding todo list

--- src/clj_scraper/examples.clj
+++ src/clj_scraper/examples.clj
@@ -3,45 +3,7 @@
             [clj-scraper.utils :refer :all])
   (:gen-class))
 
-;; Ideas
 
-;; (defentity twa-person
-;;   [nombre :finder nil]
-;;   [otra])
-
-;; (defentity edad
-;;   [])
-
-;; (defentity twa-person
-;;   [nombre :finder "/tr/td[4]/text()"]
-;;   [otra :follow fn-rest-link :finder fn]
-;;   [otroo :finder (xpath-selector "/tr/td[@class=\"statcol_num\"]/text()")])
-
-;; (defentity twa-page
-;;   [persons :splitter twa-person])
-
-
-;; (def age [{:name :age-in-number
-;;            :type :simple
-;;            :finder ""}])
-
-;; (def twa-person [{:name :position
-;;               :type :simple
-;;               :finder (xpath-selector "/tr/td[@class=\"statcol_num\"]/text()")}
-;;              {:name :name
-;;               :type :simple
-;;               :finder (xpath-selector "/tr/td[@class=\"statcol_name\"]/text()")}
-;;              {:name :age
-;;               :type :simple
-;;               :finder ""
-;;               :follow  ""}])
-
-;; (def twa-page [{:name :all
-;;                 :type :collection
-;;                 :col-type twa-person
-;;                 :limit 10
-;;                 :splitter (xpath-splitter "//*[@id=\"therest\"]/table/tbody/tr[position()>1]")
-;;                 :next-page nil}])
 
 (def people-spl (memoize (xpath-splitter "//*[@id=\"therest\"]/table/tbody/tr[position()>1]")))
 
@@ -56,7 +18,4 @@
    :limit 2
    :col-type person])
 
-(clojure.pprint/pprint (extract-url people "http://twitaholic.com/"))
-
-;;(def testp (fetch-url "http://twitaholic.com/"))
-;;(extract twa-page testp)
+;; (clojure.pprint/pprint (extract-url people "http://twitaholic.com/"))
